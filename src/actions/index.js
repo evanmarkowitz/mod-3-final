184593a05e9aa63a1cbcdd164db02eacb55b9762
Evan Markowitz <emarkowitz1@gmail.com>
2019-07-31 09:56:00 -0600

    Wrote has error reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,13 @@ export const getAnimals = (animals) => ({
   type: 'GET_ANIMALS',
   animals
 })
+
+export const hasErrored = (error) => ({
+  type: 'HAS_ERRORED',
+  error
+})
+
+export const isLoading = (isLoading) => ({
+  type: 'IS_LOADING',
+  isLoading
+})
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux'
 import { animalsReducer } from './animalsReducer'
+import { isLoadingReducer } from './isLoadingReducer'
+import { hasErroredReducer } from './hasErroredReducer';
 
 const rootReducer = combineReducers({
-  animals: animalsReducer
+  animals: animalsReducer,
+  isLoading: isLoadingReducer,
+  error: hasErroredReducer
 })
 
 export default rootReducer
